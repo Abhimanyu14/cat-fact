eeab3ca156a98b64856f39f0dda5e07a9a6f6a69
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-08-02 22:37:17 +0530

    credits and styling

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/credits/CreditsFragment.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/credits/CreditsFragment.kt
@@ -1,6 +1,7 @@
 package com.makeappssimple.abhimanyu.catfact.android.ui.credits
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,21 +9,30 @@ import androidx.fragment.app.Fragment
 import com.makeappssimple.abhimanyu.catfact.android.databinding.FragmentCreditsBinding
 
 class CreditsFragment : Fragment() {
-    
+
     private lateinit var binding: FragmentCreditsBinding
-    
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCreditsBinding.inflate(inflater)
-        
+
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
-        
+
         setHasOptionsMenu(true)
         activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
-        
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fragmentCreditsTextviewApi.movementMethod = LinkMovementMethod.getInstance()
+        binding.fragmentCreditsTextviewAppLogo.movementMethod = LinkMovementMethod.getInstance()
+        binding.fragmentCreditsTextviewNoInternetConnection.movementMethod =
+            LinkMovementMethod.getInstance()
     }
 }
