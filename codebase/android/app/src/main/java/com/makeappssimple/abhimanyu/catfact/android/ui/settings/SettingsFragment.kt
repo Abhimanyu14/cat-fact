0b4ce621f5d1cb56c0035b4be03730fc2f21aaf9
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-08-06 20:17:31 +0530

    licenses

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/settings/SettingsFragment.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/settings/SettingsFragment.kt
@@ -1,11 +1,13 @@
 package com.makeappssimple.abhimanyu.catfact.android.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.makeappssimple.abhimanyu.catfact.android.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -39,8 +41,8 @@ class SettingsFragment : Fragment() {
         }
 
         binding.fragmentSettingsTextviewLicences.setOnClickListener {
-            val action = SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
-            findNavController().navigate(action)
+            val intent = Intent(activity, OssLicensesMenuActivity::class.java)
+            activity?.startActivity(intent)
         }
     }
 }
