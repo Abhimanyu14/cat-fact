fc2d82392b01d24403869dd18db79159705c73be
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-08-06 20:40:39 +0530

    removed unused imports

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/about/AboutFragment.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/catfact/android/ui/about/AboutFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.makeappssimple.abhimanyu.catfact.android.databinding.FragmentAboutBinding
-import com.makeappssimple.abhimanyu.catfact.android.databinding.FragmentCreditsBinding
 
 class AboutFragment : Fragment() {
 
